acd043a7da4767e7625d45444c81b3e1b74efb96
SergWulf <serg.wulf@mail.ru>
2018-11-22 13:04:56 +0300

    Исправил замечания. Часть1

--- js/stat.js
+++ js/stat.js
@@ -13,7 +13,16 @@ var COLUMN_SPACING = 50;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  return 0;
+};
+
+var maxValue = function (values) {
+  var maxCurrentValue = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] > maxCurrentValue) {
+      maxCurrentValue = values[i];
+    }
+  }
+  return maxCurrentValue;
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -28,25 +37,16 @@ window.renderStatistics = function (ctx, names, times) {
   var onePercentHeightColumn = COLUMN_HEIGHT / 100;
   var onePercentMaxScorePlayer = 0; // Почему-то именно эту переменную подчеркивает редактор (Variable initializer is redundant more...)
   var columnOffset = COLUMN_WIDTH + COLUMN_SPACING;
-  var MaxScorePlayer = 0;
+  var maxScorePlayer = maxValue(times); // Найти игрока с максимальным значением
   var columnHeightPlayer = 0;
   var colorColumnPlayer = '';
   var colorBlack = '#000000';
-  // Найти игрока с максимальным значением
-  for (var i = 0; i < times.length; i++) {
-    if (times[i] > MaxScorePlayer) {
-      MaxScorePlayer = times[i];
-    }
-  }
+
   // Найти количество очков соответсвующее одному проценту от максимального значения
-  onePercentMaxScorePlayer = MaxScorePlayer / 100;
+  onePercentMaxScorePlayer = maxScorePlayer / 100;
   for (var j = 0; j < names.length; j++) {
     columnHeightPlayer = Math.floor(times[j] / onePercentMaxScorePlayer * onePercentHeightColumn);
-    if (names[j] === 'Вы') {
-      colorColumnPlayer = 'rgba(255, 0, 0, 1)';
-    } else {
-      colorColumnPlayer = 'rgba(0, 0, 255, ' + Math.random() + ')';
-    }
+    colorColumnPlayer = (names[j] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random() + ')';
     if (j === 0) {
       ctx.fillStyle = colorBlack;
       ctx.fillText(String(Math.round(times[j])), CLOUD_X + COLUMN_SPACING, CLOUD_HEIGHT - CLOUD_Y - FONT_GAP * 1.5 - columnHeightPlayer);
